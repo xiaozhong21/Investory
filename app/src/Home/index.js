a95b9b6f88431f27f53a32f9f4c1738569cba97d
Xiao Zhong <xzhong.dev@gmail.com>
2021-11-08 01:14:12 -0700

    update homepage contents

--- app/src/Home/index.js
+++ app/src/Home/index.js
@@ -25,35 +25,36 @@ const Home = () => {
         <img src={discoverIdeas} alt="" />
         <div className={styles.description}>
           <h2>Discover</h2>
-          <p>Get investment inspiration from daily gainers and top movers.</p>
+          <p>Get investment inspirations from daily gainers and top movers.</p>
           <Link to="/discover">
-            <button>Learn More</button>
+            <button>Discover Stocks</button>
           </Link>
         </div>
       </div>
       <div className={`${styles.mainSection} ${styles.backtest}`}>
-        <img src={stockChart} alt="" />
         <div className={styles.description}>
           <h2>Backtest a Portfolio</h2>
           <p>
-            Create your hypothetical portoflio of stocks to see how it has
-            performed in the time frame you specified.
+            Create your investment story by building your customized portfolio
+            of stocks and visualize how it has performed in the time frame you
+            specified.
           </p>
           <Link to="/addPortfolio">
-            <button>Learn More</button>
+            <button>Create Portfolio</button>
           </Link>
         </div>
+        <img src={stockChart} alt="" />
       </div>
       <div className={`${styles.mainSection} ${styles.myStocks}`}>
         <img src={moneyTree} alt="" />
         <div className={styles.description}>
           <h2>My Stocks</h2>
           <p>
-            This is the center hub where you can track your portoflios and
+            This is the center hub where you can track your portfolios and
             watchlist.
           </p>
           <Link to="/mystocks">
-            <button>Learn More</button>
+            <button>Start Tracking</button>
           </Link>
         </div>
       </div>
